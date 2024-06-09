c40ae3c0e8d60df84a4acb05b893c53249bdfb29
Dehayez <yentel.dh@hotmail.be>
2024-06-09 23:15:17 +0200

    updated audioplayer

--- client/src/components/AudioPlayer.js
+++ client/src/components/AudioPlayer.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import ReactPlayer from 'react-player';
 import { FiPlayCircle, FiPauseCircle } from 'react-icons/fi';
+import './AudioPlayer.css';
 
 let currentPlaying;
 
@@ -43,14 +44,34 @@ const AudioPlayer = ({ currentTrack }) => {
   };
 
   return (
-    <div style={{ position: 'fixed', bottom: 0, width: '100%', background: '#282828', color: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', padding: '20px' }}>
-      <input type='range' min={0} max={0.999999} step='any' value={progress} onMouseDown={handleSeekMouseDown} onChange={handleSeekChange} onMouseUp={handleSeekMouseUp} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)} style={{ width: '100%', position: 'absolute', top: 0, background: isHovering ? '#fff' : '#000' }} />
+    <div style={{ 
+        position: 'fixed', 
+        bottom: 0, 
+        left: 0, 
+        right: 0, 
+        height: '60px', // adjust this value as needed
+        background: '#282828', 
+        color: '#fff', 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        flexDirection: 'column', 
+        padding: '20px' 
+    }}>
+      <input type='range' min={0} max={0.999999} step='any' value={progress} onMouseDown={handleSeekMouseDown} onChange={handleSeekChange} onMouseUp={handleSeekMouseUp} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)} style={{ width: '100%', position: 'absolute', top: 0, background: isHovering ? '#fff' : '#000', cursor: 'pointer' }} />
       {isPlaying ? (
-        <FiPauseCircle size={48} onClick={() => setIsPlaying(!isPlaying)} />
+        <FiPauseCircle size={48} onClick={() => setIsPlaying(!isPlaying)} style={{ cursor: 'pointer' }} />
       ) : (
-        <FiPlayCircle size={48} onClick={() => setIsPlaying(!isPlaying)} />
+        <FiPlayCircle size={48} onClick={() => setIsPlaying(!isPlaying)} style={{ cursor: 'pointer' }} />
       )}
-      <ReactPlayer ref={playerRef} url={currentTrack && `/${currentTrack.audio}`} playing={isPlaying} onProgress={handleProgress} hidden />
+     <ReactPlayer 
+        ref={playerRef} 
+        url={currentTrack && `/${currentTrack.audio}`} 
+        playing={isPlaying} 
+        onProgress={handleProgress} 
+        progressInterval={100} // update progress every 100ms
+        hidden 
+        />
     </div>
   );
 };
