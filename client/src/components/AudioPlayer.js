d592d0b3c6a2b66beaaa76aaaf032dd8cf2de468
Dehayez <yentel.dh@hotmail.be>
2024-06-09 23:51:41 +0200

    audioplayer react-h5-audio-player

--- client/src/components/AudioPlayer.js
+++ client/src/components/AudioPlayer.js
@@ -1,78 +1,55 @@
-import React, { useState, useRef, useEffect } from 'react';
-import ReactPlayer from 'react-player';
-import { FiPlayCircle, FiPauseCircle } from 'react-icons/fi';
+import React, { useState, useEffect, useRef } from 'react';
+import H5AudioPlayer, { RHAP_UI } from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 import './AudioPlayer.css';
 
 let currentPlaying;
 
 const AudioPlayer = ({ currentTrack }) => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [progress, setProgress] = useState(0);
-  const [isHovering, setIsHovering] = useState(false);
+  const [progress, setProgress] = useState(0); // Define setProgress
   const playerRef = useRef();
 
   useEffect(() => {
     if (currentTrack && currentTrack.audio) {
       if (currentPlaying && currentPlaying !== playerRef.current) {
-        currentPlaying.getInternalPlayer().pause();
+        currentPlaying.audio.current.pause();
       }
       setIsPlaying(true);
       currentPlaying = playerRef.current;
     } else {
       setIsPlaying(false);
       if (playerRef.current) {
-        setProgress(0);
+        playerRef.current.audio.current.currentTime = 0;
       }
     }
   }, [currentTrack]);
 
-  const handleProgress = state => {
-    setProgress(state.played);
-  };
-
-  const handleSeekChange = e => {
-    setProgress(parseFloat(e.target.value));
-  };
-
-  const handleSeekMouseDown = e => {
-    setIsPlaying(false);
-  };
-
-  const handleSeekMouseUp = e => {
-    setIsPlaying(true);
-    playerRef.current.seekTo(parseFloat(e.target.value));
+  const handleListen = () => {
+    if (playerRef.current) {
+      const progress = playerRef.current.audio.current.currentTime / playerRef.current.audio.current.duration;
+      setIsPlaying(!playerRef.current.audio.current.paused);
+      if (!isNaN(progress)) {
+        setProgress(progress);
+      }
+    }
   };
 
   return (
-    <div style={{ 
-        position: 'fixed', 
-        bottom: 0, 
-        left: 0, 
-        right: 0, 
-        height: '60px', // adjust this value as needed
-        background: '#282828', 
-        color: '#fff', 
-        display: 'flex', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        flexDirection: 'column', 
-        padding: '20px' 
-    }}>
-      <input type='range' min={0} max={0.999999} step='any' value={progress} onMouseDown={handleSeekMouseDown} onChange={handleSeekChange} onMouseUp={handleSeekMouseUp} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)} style={{ width: '100%', position: 'absolute', top: 0, background: isHovering ? '#fff' : '#000', cursor: 'pointer' }} />
-      {isPlaying ? (
-        <FiPauseCircle size={48} onClick={() => setIsPlaying(!isPlaying)} style={{ cursor: 'pointer' }} />
-      ) : (
-        <FiPlayCircle size={48} onClick={() => setIsPlaying(!isPlaying)} style={{ cursor: 'pointer' }} />
-      )}
-     <ReactPlayer 
-        ref={playerRef} 
-        url={currentTrack && `/${currentTrack.audio}`} 
-        playing={isPlaying} 
-        onProgress={handleProgress} 
-        progressInterval={100} // update progress every 100ms
-        hidden 
-        />
-    </div>
+  <H5AudioPlayer
+    autoPlayAfterSrcChange={true} // Set this to true
+    src={currentTrack ? currentTrack.audio : ''}
+    ref={playerRef}
+    onPlay={() => setIsPlaying(true)}
+    onPause={() => setIsPlaying(false)}
+    onListen={handleListen}
+    customProgressBarSection={[
+      RHAP_UI.PROGRESS_BAR,
+      RHAP_UI.CURRENT_TIME,
+      RHAP_UI.DURATION
+    ]}
+    customControlsSection={isPlaying ? [RHAP_UI.MAIN_CONTROLS, RHAP_UI.VOLUME_CONTROLS] : [RHAP_UI.MAIN_CONTROLS]}
+  />
   );
 };
 
