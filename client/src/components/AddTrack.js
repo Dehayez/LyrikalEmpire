ec3f11808a1d73428f547897099f950d29e60980
Dehayez <yentel.dh@hotmail.be>
2024-06-12 01:03:31 +0200

    styled add track modal

--- client/src/components/AddTrack.js
+++ client/src/components/AddTrack.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 import { addTrack } from '../services/trackService';
+import { IoIosCloudUpload } from "react-icons/io";
+import './AddTrack.scss'
 
 Modal.setAppElement('#root');
 
@@ -12,6 +14,7 @@ const AddTrack = ({ onAdd, isOpen, setIsOpen }) => {
     const [tierlist, setTierlist] = useState('');
     const [mood, setMood] = useState('');
     const [keywords, setKeywords] = useState('');
+    const [fileName, setFileName] = useState('No file chosen');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -28,6 +31,11 @@ const AddTrack = ({ onAdd, isOpen, setIsOpen }) => {
         setIsOpen(false);
     };
 
+    const handleFileChange = (e) => {
+        setAudio(e.target.files[0]);
+        setFileName(e.target.files[0].name);
+    }
+
     const modalStyle = {
         overlay: {
             backgroundColor: 'rgba(30, 30, 30, 0.75)'
@@ -43,27 +51,35 @@ const AddTrack = ({ onAdd, isOpen, setIsOpen }) => {
         }
     };
 
+    const h2Style = {
+        marginTop: '0',
+    };
+
     return (
         <div>
             <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={modalStyle}>
+                <h2 style={h2Style}>Add Tracks</h2>
                 <form onSubmit={handleSubmit}>
-                    <div>
+                    <div className="form-group">
                         <label>Title:</label>
                         <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
                     </div>
-                    <div>
-                        <label>Audio:</label>
-                        <input type="file" onChange={(e) => setAudio(e.target.files[0])} required />
+                    <div className="form-group file-input">
+                        <label htmlFor="file" className="file-input__label">
+                            <IoIosCloudUpload /> Upload File
+                        </label>
+                        <input type="file" id="file" className="file-input__input" onChange={handleFileChange} required />
+                        <span id="file-name" className="file-input__name">{fileName}</span>
                     </div>
-                    <div>
+                    <div className="form-group">
                         <label>BPM:</label>
                         <input type="number" value={bpm} onChange={(e) => setBpm(e.target.value)} required />
                     </div>
-                    <div>
+                    <div className="form-group">
                         <label>Genre:</label>
                         <input type="text" value={genre} onChange={(e) => setGenre(e.target.value)} required />
                     </div>
-                    <div>
+                    <div className="form-group">
                         <label>Tierlist:</label>
                         <select value={tierlist} onChange={(e) => setTierlist(e.target.value)} required>
                             <option value="S">S</option>
@@ -73,17 +89,17 @@ const AddTrack = ({ onAdd, isOpen, setIsOpen }) => {
                             <option value="D">D</option>
                         </select>
                     </div>
-                    <div>
+                    <div className="form-group">
                         <label>Mood:</label>
                         <input type="text" value={mood} onChange={(e) => setMood(e.target.value)} required />
                     </div>
-                    <div>
+                    <div className="form-group">
                         <label>Keywords:</label>
                         <input type="text" value={keywords} onChange={(e) => setKeywords(e.target.value)} required />
                     </div>
                     <div>
-                        <button type="submit">Add Track</button>
-                        <button type="button" onClick={() => setIsOpen(false)}>Cancel</button>
+                        <button className="modal__button modal__button--add" type="submit">Add Track</button>
+                        <button className="modal__button" type="button" onClick={() => setIsOpen(false)}>Cancel</button>
                     </div>
                 </form>
             </Modal>
