5e42f23cc43d63d1f4045547d749e622a5e5a60b
Dehayez <yentel.dh@hotmail.be>
2024-06-11 14:57:39 +0200

    branding layout update

--- client/src/components/TrackList.js
+++ client/src/components/TrackList.js
@@ -40,15 +40,15 @@ const TrackList = ({ onPlay, selectedTrack, isPlaying }) => {
   };
 
   const styles = {
-    tableContainer: { overflowX: 'auto' },
+    tableContainer: { overflowX: 'auto', backgroundColor: '#202020', color: '#FFFFFF' },
     table: { minWidth: '600px', width: '100%', tableLayout: 'auto' },
-    thead: { position: 'sticky', top: 0, backgroundColor: '#fff', textAlign: 'left' },
-    tdata: { padding: '14px', paddingLeft: '0'},
+    thead: { position: 'sticky', top: 0, backgroundColor: '#202020', color: '#FFFFFF', textAlign: 'left' },
+    tdata: { padding: '14px', paddingLeft: '0', color: '#FFFFFF'},
     numberColumnCell: { paddingLeft: '14px'},
     theadFirstChild: { textAlign: 'center'},
-    th: { padding: '10px', paddingLeft: '0'},
+    th: { padding: '10px', paddingLeft: '0', color: '#FFFFFF'},
     buttonCell: { position: 'relative', width: '100%', height: '100%'},
-    playPauseButton: { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', opacity: 0, zIndex: 2 },
+    playPauseButton: { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', opacity: 0, zIndex: 2, color: '#FFCC44' },
   };
 
   return (
