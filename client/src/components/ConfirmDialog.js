78594f54aaa9809b5ac9a36a28443293d60090f4
Dehayez <yentel.dh@hotmail.be>
2024-06-11 23:31:52 +0200

    styled confirmdialog

--- client/src/components/ConfirmDialog.js
+++ client/src/components/ConfirmDialog.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import Modal from 'react-modal';
+import { IoCloseSharp } from "react-icons/io5";
+import './ConfirmDialog.scss';
 
 Modal.setAppElement('#root');
 
 const closeButtonStyle = {
   position: 'absolute', 
   right: '10px', 
-  top: '10px'
+  top: '10px',
+  color: 'white',
+  backgroundColor: '#181818',
+  border: 'none'
+};
+
+const h2Style = {
+  marginTop: '0',
+};
+
+const modalStyle = {
+  overlay: {
+    backgroundColor: 'rgba(30, 30, 30, 0.75)'
+  },
+  content: {
+    backgroundColor: '#181818',
+    color: 'white', 
+    border: 'none',
+    width: '50%',
+    height: '14%',
+    margin: 'auto', 
+  }
+};
+
+const buttonStyle = {
+  backgroundColor: '#202020',
+  color: 'white',
+  border: 'none',
+  padding: '10px 20px',
+  margin: '10px 10px 10px 0',
+  borderRadius: '5px',
+  cursor: 'pointer',
+  transition: 'all 0.3s ease'
 };
 
 const ConfirmDialog = ({ isOpen, message, onConfirm, onCancel }) => (
-  <Modal isOpen={isOpen} onRequestClose={onCancel}>
-    <button onClick={onCancel} style={closeButtonStyle}>X</button>
-    <h2>Confirm</h2>
+  <Modal isOpen={isOpen} onRequestClose={onCancel} style={modalStyle}>
+    <button onClick={onCancel} style={closeButtonStyle}>
+      <IoCloseSharp style={{ fontSize: '18px' }} />
+    </button>
+    <h2 style={h2Style}>Confirm</h2>
     <p>{message}</p>
-    <button onClick={onConfirm}>Delete</button>
-    <button onClick={onCancel}>Cancel</button>
+    <button className="dialog-button" onClick={onConfirm} style={buttonStyle}>Delete</button>
+    <button className="dialog-button" onClick={onCancel} style={buttonStyle}>Cancel</button>
   </Modal>
 );
 
