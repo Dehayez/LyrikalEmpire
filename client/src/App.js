1f5353edb4e856d17f6e24803399b0c97e484080
Dehayez <yentel.dh@hotmail.be>
2024-06-13 23:37:13 +0200

    better filestructure: placed audioplayer code to the audioplayer component

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,7 @@ import BeatList from './components/BeatList';
 import AddBeatForm from './components/AddBeat/AddBeatForm';
 import AddBeatButton from './components/AddBeat/AddBeatButton';
 import AudioPlayer from './components/AudioPlayer/AudioPlayer';
-import { IoAdd, IoVolumeHigh, IoVolumeMedium, IoVolumeLow, IoVolumeOff } from 'react-icons/io5';
-
+import { IoAdd } from 'react-icons/io5';
 import './App.scss';
 
 const styles = {
@@ -27,6 +26,7 @@ function App() {
   const [addBeatButtonBottom, setAddBeatButtonBottom] = useState(20);
   const [animateAddButton, setAnimateAddButton] = useState(false);
   const [volume, setVolume] = useState(1.0); // Volume is between 0.0 and 1.0
+
   const handleAdd = () => setRefresh(!refresh);
   const handlePlay = (beat, play, beats) => {
     setSelectedBeat(beat);
@@ -51,6 +51,7 @@ function App() {
     const prevIndex = (currentIndex - 1 + beats.length) % beats.length;
     handlePlay(beats[prevIndex], true, beats);
   };
+
   useEffect(() => {
     const audioPlayer = document.getElementById('audio-player');
     const mainContent = document.getElementById('main-content');
@@ -61,6 +62,7 @@ function App() {
       setAddBeatButtonBottom(audioPlayerHeight + 20);
     }
   }, [currentBeat]);
+
   return (
     <div className="App">
       <div id="main-content" style={styles.container}>
@@ -95,39 +97,7 @@ function App() {
           <IoAdd />
         </button>
       </div>
-      <div id="audio-player" style={{
-        display: 'flex', 
-        alignItems: 'center', 
-        position: 'fixed', 
-        bottom: currentBeat ? 0 : '-100%',
-        width: '100%',
-        transition: 'bottom 0.4s cubic-bezier(0.25, 0.1, 0.25, 1)',
-        padding: '0 20px',
-        boxSizing: 'border-box',
-        backgroundColor: '#181818',
-      }}>
-        <div style={{ flex: '1' }}>
-          {currentBeat && <div>{currentBeat.title}</div>}
-        </div>
-        <div style={{ flex: '2' }}>
-          {currentBeat && <AudioPlayer currentBeat={currentBeat} isPlaying={isPlaying} setIsPlaying={setIsPlaying} onNext={handleNext} onPrev={handlePrev} />}
-        </div>
-        <div className='audio-player__volume' style={{ flex: '1' }}>
-          {volume > 0.66 && <IoVolumeHigh />}
-          {volume > 0.33 && volume <= 0.66 && <IoVolumeMedium />}
-          {volume > 0 && volume <= 0.33 && <IoVolumeLow />}
-          {volume === 0 && <IoVolumeOff />}
-          <input 
-            type="range" 
-            className='volume-slider'
-            min="0" 
-            max="1" 
-            step="0.01" 
-            value={volume} 
-            onChange={e => setVolume(parseFloat(e.target.value))} 
-          />
-        </div>
-      </div>
+      <AudioPlayer currentBeat={currentBeat} isPlaying={isPlaying} setIsPlaying={setIsPlaying} onNext={handleNext} onPrev={handlePrev} volume={volume} setVolume={setVolume} />
     </div>
   );
 }
