c155b4d7b964e4dc8261cde97565145a504fbf5f
Dehayez <yentel.dh@hotmail.be>
2024-06-15 01:57:14 +0200

    seperated code for better structure

--- client/src/components/BeatList/BeatList.js
+++ client/src/components/BeatList/BeatList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getBeats, deleteBeat, updateBeat } from '../../services/beatService';
 import ConfirmModal from '../ConfirmModal';
-import { IoPlaySharp, IoPauseSharp, IoTrashBinOutline } from "react-icons/io5";
+import BeatRow from './BeatRow';
 import './BeatList.scss';
 
 const BeatList = ({ onPlay, selectedBeat, isPlaying }) => {
@@ -81,66 +81,18 @@ const BeatList = ({ onPlay, selectedBeat, isPlaying }) => {
           </thead>
           <tbody>
             {beats.map((beat, index) => (
-              <tr
-              className="beat-row"
-              key={beat.id}
-              onMouseEnter={(e) => {
-                e.currentTarget.querySelector('button').style.opacity = 1;
-                setHoveredBeat(beat.id);
-              }}
-              onMouseLeave={(e) => {
-                e.currentTarget.querySelector('button').style.opacity = 0;
-                setHoveredBeat(null);
-              }}
-            >
-                <td style={{ ...styles.tdata, ...styles.numberColumnCell }} className="beat-number">
-                  <div style={styles.buttonCell}>
-                    {selectedBeat && selectedBeat.id === beat.id && isPlaying ? 
-                      <div className="animation-container" style={{ animationDuration: `${60 / beat.bpm}s`, opacity: hoveredBeat === beat.id ? 0 : 1 }}>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                      </div> : 
-                      <div style={{ zIndex: 1, color: selectedBeat && selectedBeat.id === beat.id ? '#FFCC44' : '', opacity: hoveredBeat === beat.id ? 0 : 1 }}>{index + 1}</div>
-                    }
-                    <button
-                      style={styles.playPauseButton}
-                      className="icon-button"
-                      onClick={() => handlePlayPause(beat)}
-                    >
-                      {playingBeat && playingBeat.id === beat.id && isPlaying ? <IoPauseSharp /> : <IoPlaySharp />}
-                      <span className="tooltip">
-                        {playingBeat && playingBeat.id === beat.id && isPlaying ? 'Pause' : 'Play'}
-                      </span>
-                    </button>
-                  </div>
-                </td>
-                <td style={{ color: selectedBeat && selectedBeat.id === beat.id ? '#FFCC44' : '' }}>
-                  <input type="text" defaultValue={beat.title} onBlur={(e) => handleUpdate(beat.id, 'title', e.target.value)} />
-                </td>
-                <td style={styles.tdata}>
-                  <input type="text" defaultValue={beat.genre} onBlur={(e) => handleUpdate(beat.id, 'genre', e.target.value)} />
-                </td>
-                <td style={styles.tdata}>
-                  <input type="text" defaultValue={beat.bpm} onBlur={(e) => handleUpdate(beat.id, 'bpm', e.target.value)} />
-                </td>
-                <td style={styles.tdata}>
-                  <input type="text" defaultValue={beat.tierlist} onBlur={(e) => handleUpdate(beat.id, 'tierlist', e.target.value)} />
-                </td>
-                <td style={styles.tdata}>
-                  <input type="text" defaultValue={beat.mood} onBlur={(e) => handleUpdate(beat.id, 'mood', e.target.value)} />
-                </td>
-                <td style={styles.tdata}>
-                  <input type="text" defaultValue={beat.keywords} onBlur={(e) => handleUpdate(beat.id, 'keywords', e.target.value)} />
-                </td>
-                <td style={styles.tdata}>
-                  <button className="icon-button" onClick={() => handleDelete(beat.id)}>
-                    <IoTrashBinOutline />
-                    <span className="tooltip">Delete</span>
-                  </button>
-                </td>
-              </tr>
+              <BeatRow
+                key={beat.id}
+                beat={beat}
+                index={index}
+                handlePlayPause={handlePlayPause}
+                handleUpdate={handleUpdate}
+                handleDelete={handleDelete}
+                selectedBeat={selectedBeat}
+                isPlaying={isPlaying}
+                hoveredBeat={hoveredBeat}
+                setHoveredBeat={setHoveredBeat}
+              />
             ))}
           </tbody>
         </table>
