88441c7296317608234006fddc192ae496da9261
Dehayez <yentel.dh@hotmail.be>
2024-06-15 16:30:06 +0200

    updated animation style

--- client/src/components/BeatList/BeatAnimation.js
+++ client/src/components/BeatList/BeatAnimation.js
@@ -7,13 +7,13 @@ const BeatAnimation = ({ beat, selectedBeat, isPlaying, hoveredBeat, index }) =>
   const [durations, setDurations] = useState([0, 0, 0, 0]);
 
   useEffect(() => {
-    setDelays([0.5, 1, 1.5, 2]);
-    setDurations([1.5, 2, 2.5, 3]);
+    setDelays([Math.random(), Math.random(), Math.random(), Math.random()]);
+    setDurations([1 + Math.random(), 1 + Math.random(), 1 + Math.random(), 1 + Math.random()]);
   }, []);
 
   return (
     selectedBeat && selectedBeat.id === beat.id && isPlaying ? 
-    <div className="animation-container" style={{ animation: `barAnimation ${120 / beat.bpm}s infinite`, opacity: hoveredBeat === beat.id ? 0 : 1 }}>
+    <div className="animation-container" style={{ animation: `barAnimation ${170 / beat.bpm}s infinite`, opacity: hoveredBeat === beat.id ? 0 : 1 }}>
         <div className="bar" style={{ animationDelay: `-${delays[0]}s`, animationDuration: `${durations[0]}s` }}></div>
         <div className="bar" style={{ animationDelay: `-${delays[1]}s`, animationDuration: `${durations[1]}s` }}></div>
         <div className="bar" style={{ animationDelay: `-${delays[2]}s`, animationDuration: `${durations[2]}s` }}></div>
