153ac690b4c67a0121ed82aee9f7de6a5fcb4567
Dehayez <yentel.dh@hotmail.be>
2024-06-18 13:50:57 +0200

    scss back to js

--- client/src/components/ConfirmModal.js
+++ client/src/components/ConfirmModal.js
@@ -9,13 +9,38 @@ const ConfirmModal = ({ isOpen, message, onConfirm, onCancel }) => (
   <Modal 
     isOpen={isOpen} 
     onRequestClose={onCancel} 
-    className="modal"
+    style={{
+      overlay: {
+        backgroundColor: 'rgba(30, 30, 30, 0.75)',
+        zIndex: 2,
+      },
+      content: {
+        backgroundColor: '#181818',
+        color: 'white', 
+        border: 'none',
+        borderRadius: '6px',
+        maxWidth: '380px',
+        margin: 'auto', 
+        padding: '20px',
+        display: 'inline-table'
+      }
+    }}
   >
-    <button onClick={onCancel} className="modal__close-button">
-      <IoCloseSharp />
+    <button 
+      onClick={onCancel} 
+      style={{
+        position: 'absolute', 
+        right: '10px', 
+        top: '10px',
+        color: 'white',
+        backgroundColor: '#181818',
+        border: 'none'
+      }}
+    >
+      <IoCloseSharp style={{ fontSize: '18px' }} />
     </button>
-    <h2 className="modal__title">Confirm</h2>
-    <p className="modal__message">{message}</p>
+    <h2 style={{ marginTop: '0' }}>Confirm</h2>
+    <p>{message}</p>
     <button className="modal__button modal__button--delete" onClick={onConfirm}>Delete</button>
     <button className="modal__button" onClick={onCancel}>Cancel</button>
   </Modal>
