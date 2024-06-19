fb3dfc660b4a3c7d783adb7d9c5ec1ea65b16b0a
Dehayez <yentel.dh@hotmail.be>
2024-06-19 22:41:35 +0200

    cleared header for now

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,11 +4,11 @@ import './Header.scss';
 const Header = () => (
   <header className="header">
     <div className="header__nav-group">
-      <a className="header__nav-link" href="/">Home</a>
-      <a className="header__nav-link" href="/beats">Beats</a>
+      <a className="header__nav-link" href="/"></a>
+      <a className="header__nav-link" href="/beats"></a>
       <img className="header__nav-logo" src="/android-chrome-192x192.png" alt="Logo" />
-      <a className="header__nav-link" href="/info">Info</a>
-      <a className="header__nav-link" href="/contact">Contact</a>
+      <a className="header__nav-link" href="/info"></a>
+      <a className="header__nav-link" href="/contact"></a>
     </div>
   </header>
 );
