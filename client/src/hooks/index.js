0ef454f8d17bea75556a88fd97b24c2dc7a67941
Dehayez <yentel.dh@hotmail.be>
2024-06-22 18:22:05 +0200

    seperated file for functions

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -3,3 +3,4 @@ export { useBeatActions } from './useBeatActions';
 export { handlePlay, handleNext, handlePrev } from './audioControls';
 export { useResizableColumns } from './useResizableColumns';
 export { useBpmHandlers } from './useBpmHandlers';
+export { useLocalStorageAndFetch } from './useLocalStorageAndFetch';
