2b84b1021325c32a36477c1388631c897dfbb43f
Dehayez <yentel.dh@hotmail.be>
2024-06-22 17:27:50 +0200

    removed animation on addbeat button

--- client/src/components/AddBeat/AddBeatButton.js
+++ client/src/components/AddBeat/AddBeatButton.js
@@ -2,32 +2,29 @@ import React from 'react';
 import { IoAdd } from 'react-icons/io5';
 import './AddBeatButton.scss';
 
-const AddBeatButton = ({ setIsOpen, addBeatButtonBottom, animateAddButton, setAnimateAddButton }) => (
+const AddBeatButton = ({ setIsOpen }) => (
   <button 
     style={{
       position: 'fixed', 
-      bottom: `${addBeatButtonBottom}px`, 
-      right: '20px', 
+      bottom: '130px', 
+      right: '30px', 
       transition: 'bottom 0.4s cubic-bezier(0.25, 0.1, 0.25, 1) 0.1s',
       backgroundColor: '#505050',
       color: '#fff',
       border: 'none',
       borderRadius: '50%',
-      width: '50px',
-      height: '50px',
+      width: '48px',
+      height: '48px',
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
       fontSize: '24px',
     }} 
     onClick={() => setIsOpen(true)}
-    className={`icon-button ${animateAddButton ? 'animate-scale' : ''}`} 
-    onMouseDown={() => setAnimateAddButton(true)}
-    onMouseUp={() => setAnimateAddButton(false)}
-    onMouseLeave={() => setAnimateAddButton(false)}
+    className="icon-button"
   >
     <span className="tooltip tooltip__addbeat">Add Beat</span>
-    <IoAdd />
+    <IoAdd/>
   </button>
 );
 
