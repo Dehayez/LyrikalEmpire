251b75bfa5633cb78c20089bb4548d02ad2d9ded
Dehayez <yentel.dh@hotmail.be>
2024-06-25 19:34:35 +0200

    update the styling of the baet that is being played

--- client/src/components/BeatList/BeatAnimation.js
+++ client/src/components/BeatList/BeatAnimation.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './BeatAnimation.scss';
 
-const BeatAnimation = ({ beat, selectedBeat, isPlaying, hoveredBeat, index }) => {
+const BeatAnimation = ({ beat, currentBeat, isPlaying, hoveredBeat, index }) => {
   
   const [delays, setDelays] = useState([0, 0, 0, 0]);
   const [durations, setDurations] = useState([0, 0, 0, 0]);
@@ -12,14 +12,14 @@ const BeatAnimation = ({ beat, selectedBeat, isPlaying, hoveredBeat, index }) =>
   }, []);
 
   return (
-    selectedBeat && selectedBeat.id === beat.id && isPlaying ? 
+    currentBeat && currentBeat.id === beat.id && isPlaying ? 
     <div className="animation-container" style={{ animation: `barAnimation ${170 / beat.bpm}s infinite`, opacity: hoveredBeat === beat.id ? 0 : 1 }}>
         <div className="bar" style={{ animationDelay: `-${delays[0]}s`, animationDuration: `${durations[0]}s` }}></div>
         <div className="bar" style={{ animationDelay: `-${delays[1]}s`, animationDuration: `${durations[1]}s` }}></div>
         <div className="bar" style={{ animationDelay: `-${delays[2]}s`, animationDuration: `${durations[2]}s` }}></div>
         <div className="bar" style={{ animationDelay: `-${delays[3]}s`, animationDuration: `${durations[3]}s` }}></div>
       </div> : 
-      <div className='beat-row__index-number' style={{ zIndex: 1, minWidth: '30px', color: selectedBeat && selectedBeat.id === beat.id ? '#FFCC44' : '', opacity: hoveredBeat === beat.id ? 0 : 1 }}>{index + 1}</div>
+      <div className='beat-row__index-number' style={{ zIndex: 1, minWidth: '30px', color: currentBeat && currentBeat.id === beat.id ? '#FFCC44' : '', opacity: hoveredBeat === beat.id ? 0 : 1 }}>{index + 1}</div>
   );
 };
 
