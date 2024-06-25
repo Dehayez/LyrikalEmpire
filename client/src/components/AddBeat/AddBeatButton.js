1e7363623a490855887eac00b6baeb20f391f8cb
Dehayez <yentel.dh@hotmail.be>
2024-06-25 15:14:52 +0200

    sync animation

--- client/src/components/AddBeat/AddBeatButton.js
+++ client/src/components/AddBeat/AddBeatButton.js
@@ -3,13 +3,13 @@ import { IoAdd } from 'react-icons/io5';
 import './AddBeatButton.scss';
 
 function AddBeatButton({ setIsOpen, isRightPanelVisible, isRightDivVisible }) {
-  const buttonStyle = isRightPanelVisible || isRightDivVisible ? { right: '380px' } : { right: '30px' };
+  const buttonClass = isRightPanelVisible || isRightDivVisible ? 'button-slide-in' : 'button-slide-out';
 
   return (
-    <button className="icon-button icon-button--addbeat" style={buttonStyle} onClick={() => setIsOpen(true)}>
-      <span className="tooltip tooltip__addbeat">Add Beat</span>
-      <IoAdd/>
-    </button>
+    <button className={`icon-button icon-button--addbeat ${buttonClass}`} onClick={() => setIsOpen(true)}>
+    <span className="tooltip tooltip__addbeat">Add Beat</span>
+    <IoAdd/>
+  </button>
   );
 }
 
