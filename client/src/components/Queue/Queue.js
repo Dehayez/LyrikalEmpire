79daf31e86a21c8ac4a1757ac464bf929acab5b0
Dehayez <yentel.dh@hotmail.be>
2024-06-25 06:05:31 +0200

    queue update

--- client/src/components/Queue/Queue.js
+++ client/src/components/Queue/Queue.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './Queue.scss';
 
-const Queue = ({ queue }) => {
+const Queue = ({ queue, currentBeat }) => {
   return (
     <div className="queue">
       <h2 className="queue__title">Queue</h2>
-      <ul className="queue__list">
-        {queue.map((beat, index) => (
-          <li className='queue__list-item' key={index}>{beat.title}</li>
-        ))}
-      </ul>
+      {queue.length > 0 && (
+        <>
+          <h3 className="queue__subtitle">Now Playing</h3>
+          <ul className="queue__list">
+            <li className={`queue__list-item queue__list-item--playing`} key={queue[0].id}>
+              {queue[0].title}
+            </li>
+          </ul>
+        </>
+      )}
+      {queue.length > 1 && (
+        <>
+          <h3 className="queue__subtitle">Next</h3>
+          <ul className="queue__list">
+            {queue.slice(1).map((beat, index) => (
+              <li className={`queue__list-item ${currentBeat && beat.id === currentBeat.id ? 'queue__list-item--playing' : ''}`} key={beat.id}>
+                {beat.title}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
