850a5bc0fd7e6f5a839a75d63beee9d88080d17b
Dehayez <yentel.dh@hotmail.be>
2024-06-27 01:31:49 +0200

    header sort up, down, default icon

--- client/src/components/BeatList/TableHeader.js
+++ client/src/components/BeatList/TableHeader.js
@@ -1,8 +1,9 @@
 import React, { useState, useRef } from 'react';
 import './TableHeader.scss';
 import { useResizableColumns } from '../../hooks';
+import { IoChevronUpSharp, IoChevronDownSharp } from 'react-icons/io5';
 
-const TableHeader = ({ onSort }) => {
+const TableHeader = ({ onSort, sortConfig }) => {
   const tableRef = useRef(null);
   const [isDragging, setIsDragging] = useState(false);
 
@@ -14,16 +15,35 @@ const TableHeader = ({ onSort }) => {
     }
   };
 
+  const renderSortIcon = (columnName) => {
+    if (sortConfig.key === columnName) {
+      return sortConfig.direction === 'ascending' ? <IoChevronUpSharp /> : <IoChevronDownSharp />;
+    }
+    return null;
+  };
+
   return (
     <thead className="table-header" ref={tableRef}>
       <tr>
-        <th onClick={() => handleSort('number')} className={`table-header__cell table-header__cell--first ${isDragging ? 'no-transition' : ''}`}>#</th>
-        <th onClick={() => handleSort('title')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Title</th>
-        <th onClick={() => handleSort('genre')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Genre</th>
-        <th onClick={() => handleSort('bpm')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>BPM</th>
-        <th onClick={() => handleSort('tierlist')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Tierlist</th>
-        <th onClick={() => handleSort('mood')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Mood</th>
-        <th onClick={() => handleSort('keywords')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Keywords</th>
+        <th className={`table-header__cell table-header__cell--first ${isDragging ? 'no-transition' : ''}`}>#</th>
+        <th onClick={() => handleSort('title')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>
+          Title{renderSortIcon('title')}
+        </th>
+        <th onClick={() => handleSort('genre')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>
+          Genre{renderSortIcon('genre')}
+        </th>
+        <th onClick={() => handleSort('bpm')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>
+          BPM{renderSortIcon('bpm')}
+        </th>
+        <th onClick={() => handleSort('tierlist')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>
+          Tierlist{renderSortIcon('tierlist')}
+        </th>
+        <th onClick={() => handleSort('mood')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>
+          Mood{renderSortIcon('mood')}
+        </th>
+        <th onClick={() => handleSort('keywords')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>
+          Keywords{renderSortIcon('keywords')}
+        </th>
       </tr>
     </thead>
   );
