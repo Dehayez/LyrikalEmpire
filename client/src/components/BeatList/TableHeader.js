1ce3329814daf02cd6ce0ad80c67381d695d01a5
Dehayez <yentel.dh@hotmail.be>
2024-06-27 00:59:46 +0200

    order songs with headers

--- client/src/components/BeatList/TableHeader.js
+++ client/src/components/BeatList/TableHeader.js
@@ -2,22 +2,28 @@ import React, { useState, useRef } from 'react';
 import './TableHeader.scss';
 import { useResizableColumns } from '../../hooks';
 
-const TableHeader = () => {
+const TableHeader = ({ onSort }) => {
   const tableRef = useRef(null);
   const [isDragging, setIsDragging] = useState(false);
 
   useResizableColumns(tableRef, setIsDragging);
 
+  const handleSort = (columnName) => {
+    if (onSort) {
+      onSort(columnName);
+    }
+  };
+
   return (
     <thead className="table-header" ref={tableRef}>
       <tr>
-        <th className={`table-header__cell table-header__cell--first ${isDragging ? 'no-transition' : ''}`}>#</th>
-        <th className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Title</th>
-        <th className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Genre</th>
-        <th className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>BPM</th>
-        <th className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Tierlist</th>
-        <th className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Mood</th>
-        <th className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Keywords</th>
+        <th onClick={() => handleSort('number')} className={`table-header__cell table-header__cell--first ${isDragging ? 'no-transition' : ''}`}>#</th>
+        <th onClick={() => handleSort('title')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Title</th>
+        <th onClick={() => handleSort('genre')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Genre</th>
+        <th onClick={() => handleSort('bpm')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>BPM</th>
+        <th onClick={() => handleSort('tierlist')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Tierlist</th>
+        <th onClick={() => handleSort('mood')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Mood</th>
+        <th onClick={() => handleSort('keywords')} className={`table-header__cell ${isDragging ? 'no-transition' : ''}`}>Keywords</th>
       </tr>
     </thead>
   );
