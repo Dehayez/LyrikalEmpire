5b0cb804085979d53bf95aff18d424a6f959ac19
Dehayez <yentel.dh@hotmail.be>
2024-07-01 21:02:14 +0200

    history functionality

--- client/src/components/History/History.js
+++ client/src/components/History/History.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './History.scss';
 
-const History = () => {
+const History = ({ onBeatClick, currentBeat }) => {
   const history = JSON.parse(localStorage.getItem('playedBeatsHistory') || '[]');
+  const handleBeatClick = (beat) => {
+    if (onBeatClick) {
+      onBeatClick(beat);
+    }
+  };
 
   return (
     <div className="history">
        <ul className='history__list'>
           {history.map((beat, index) => (
-            <li className='history__list-item' key={index}>{beat.title}</li>
+            <li 
+            className={`history__list-item ${currentBeat && beat.id === currentBeat.id ? 'history__list-item--playing' : ''}`}
+              key={index} onClick={() => handleBeatClick(beat)}
+            >
+              {beat.title}
+            </li>
           ))}
         </ul>
     </div>
