96643780e0effecef7b03f5e211d0c3b726f1eed
Dehayez <yentel.dh@hotmail.be>
2024-07-08 19:05:03 +0200

    playpausebutton scss

--- client/src/components/BeatList/PlayPauseButton.js
+++ client/src/components/BeatList/PlayPauseButton.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import { IoPlaySharp, IoPauseSharp } from "react-icons/io5";
-
-const styles = {
-  playPauseButton: { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', opacity: 0, zIndex: 2 },
-};
+import './PlayPauseButton.scss';
 
 const PlayPauseButton = ({ beat, handlePlayPause, currentBeat, isPlaying }) => {
   return (
-    <button
-      style={styles.playPauseButton}
-      className="icon-button icon-button--play-pause"
-      onClick={() => handlePlayPause(beat)}
-    >
+    <button className="icon-button icon-button--play-pause" onClick={() => handlePlayPause(beat)}>
       {currentBeat && currentBeat.id === beat.id && isPlaying ? <IoPauseSharp fontSize={18} /> : <IoPlaySharp fontSize={18} />}
       <span className="tooltip">
         {currentBeat && currentBeat.id === beat.id && isPlaying ? 'Pause' : 'Play'}
