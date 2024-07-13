23cbfb6a90d124e45b1971ee8277352d71df76f4
Dehayez <yentel.dh@hotmail.be>
2024-07-13 12:21:34 +0200

    unfocus upload label when submitting

--- client/src/components/Inputs/FileInput.js
+++ client/src/components/Inputs/FileInput.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import { IoCloudUploadSharp } from "react-icons/io5";
-import './Inputs.scss';
+import React, { useRef } from 'react';
+import { IoCloudUploadSharp } from 'react-icons/io5';
 
-export const FileInput = ({ fileName, onChange }) => (
-    <div className="form-group">
-        <label>Audio</label>
-        <div className="file-input">
-            <div className="file-input__wrapper">
-                <label htmlFor="file" className="file-input__label no-margin" tabIndex="0">
-                    <IoCloudUploadSharp /> Upload File
-                </label>
-                <input type="file" id="file" className="file-input__input" onChange={onChange} required accept="audio/*" />
-                <span id="file-name" className="file-input__name">{fileName}</span>
+export const FileInput = ({ fileName, onChange }) => {
+    const labelRef = useRef(null);
+
+    const handleChange = (event) => {
+        onChange(event);
+        if (labelRef.current) {
+            labelRef.current.blur();
+        }
+    };
+
+    return (
+        <div className="form-group">
+            <label>Audio</label>
+            <div className="file-input">
+                <div className="file-input__wrapper">
+                    <label htmlFor="file" className="file-input__label no-margin" tabIndex="0" ref={labelRef}>
+                        <IoCloudUploadSharp /> Upload File
+                    </label>
+                    <input type="file" id="file" className="file-input__input" onChange={handleChange} required accept="audio/*" />
+                    <span id="file-name" className="file-input__name">{fileName}</span>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
