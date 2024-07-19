123f91feba5ce8d4b07a1761464a51eda8aca266
Dehayez <yentel.dh@hotmail.be>
2024-07-19 23:16:26 +0200

    update playlist form

--- client/src/components/Playlists/UpdatePlaylistForm.js
+++ client/src/components/Playlists/UpdatePlaylistForm.js
@@ -17,13 +17,15 @@ export const UpdatePlaylistForm = ({ playlist, onClose, onUpdated }) => {
     };
   
     return (
-        <div className="update-playlist-overlay">
-        <div className="update-playlist-form">
-          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" />
-          <textarea value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description"></textarea>
-          <button onClick={handleUpdate}>Update</button>
-          <button onClick={onClose}>Cancel</button>
+        <div className='update-playlist'>
+            <div className="update-playlist__overlay" onClick={onClose}>
+                <div className="update-playlist__form" onClick={(e) => e.stopPropagation()}>
+                    <input type="text" className="update-playlist__input update-playlist__input--title" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" />
+                    <textarea className="update-playlist__textarea" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description"></textarea>
+                    <button className="update-playlist__button update-playlist__button--update" onClick={handleUpdate}>Update</button>
+                    <button className="update-playlist__button update-playlist__button--cancel" onClick={onClose}>Cancel</button>
+                </div>
+            </div>
         </div>
-      </div>
     );
   };
