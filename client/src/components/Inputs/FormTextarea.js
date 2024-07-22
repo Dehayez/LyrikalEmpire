e296d188dd8e9b340e3f6a29bcae5a4164f7d236
Dehayez <yentel.dh@hotmail.be>
2024-07-22 22:14:06 +0200

    update textarea

--- client/src/components/Inputs/FormTextarea.js
+++ client/src/components/Inputs/FormTextarea.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Inputs.scss';
 
-export const FormTextarea = ({ label, placeholder, value, onChange, required, rows, maxLength }) => (
-    <div className="form-group">
-        <label>{label}</label>
-        <textarea
-            className='form-group__input'
-            placeholder={placeholder}
-            value={value}
-            onChange={onChange}
-            required={required}
-            rows={rows}
-            maxLength={maxLength}
-        />
-    </div>
-);
+export const FormTextarea = ({ label, placeholder, value, onChange, required, rows, maxLength }) => {
+    const textareaRef = useRef(null);
+    const [remainingChars, setRemainingChars] = useState(maxLength);
+
+    useEffect(() => {
+        const textarea = textareaRef.current;
+        if (textarea) {
+            textarea.style.height = 'inherit';
+            textarea.style.height = `${textarea.scrollHeight}px`; 
+        }
+        setRemainingChars(maxLength - value.length);
+    }, [value, maxLength]);
+
+    return (
+        <div className="form-group form-textarea">
+            <label>{label}</label>
+            <textarea
+                ref={textareaRef}
+                className='form-group__input form-textarea__input'
+                placeholder={placeholder}
+                value={value}
+                onChange={(e) => {
+                    onChange(e);
+                }}
+                required={required}
+                rows={rows}
+                maxLength={maxLength}
+            />
+            <span className='form-textarea__remaining-chars'>
+                {remainingChars}
+            </span>
+        </div>
+    );
+};
