e3f7fc077f44c0ab1abfd7f514714b1b2a30e44b
Dehayez <yentel.dh@hotmail.be>
2024-07-23 19:12:34 +0200

    input warning update

--- client/src/components/Inputs/FormInput.js
+++ client/src/components/Inputs/FormInput.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Inputs.scss';
 
-export const FormInput = ({ label, type, placeholder, value, onChange, required, min, pattern, maxLength, className }) => (
+export const FormInput = ({ label, type, placeholder, value, onChange, required, min, pattern, maxLength, className, isWarning }) => (
     <div className={`form-group ${className || ''}`}>
         <label>{label}</label>
-        <input className='form-group__input' type={type} placeholder={placeholder} value={value} onChange={onChange} required={required} min={min} pattern={pattern} maxLength={maxLength} />
+        <input className={`form-group__input ${isWarning ? 'form-group__input--warning' : ''}`} type={type} placeholder={placeholder} value={value} onChange={onChange} required={required} min={min} pattern={pattern} maxLength={maxLength} />
     </div>
 );
