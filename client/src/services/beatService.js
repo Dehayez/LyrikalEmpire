be1df8d9f315b5371f2a34de6530ba6436dadf63
Dehayez <yentel.dh@hotmail.be>
2024-07-31 04:15:05 +0200

    genres, moods, keywords, features many to many api endpoints and services

--- client/src/services/beatService.js
+++ client/src/services/beatService.js
@@ -15,7 +15,7 @@ export const addBeat = async (beat, audioFile) => {
     if (beat[key] !== null) {
         formData.append(key, beat[key]);
     }
-}
+  }
 
   if (audioFile) {
     formData.append('audio', audioFile, audioFile.name);
@@ -43,4 +43,46 @@ export const deleteBeat = async (id) => {
     console.error('Failed to delete beat:', error);
     throw error;
   }
+};
+
+// Add associations
+export const addBeatKeywords = async (beatId, keywordIds) => {
+  const { data } = await axios.post(`${API_URL}/${beatId}/keywords`, { keywordIds });
+  return data;
+};
+
+export const addBeatGenres = async (beatId, genreIds) => {
+  const { data } = await axios.post(`${API_URL}/${beatId}/genres`, { genreIds });
+  return data;
+};
+
+export const addBeatMoods = async (beatId, moodIds) => {
+  const { data } = await axios.post(`${API_URL}/${beatId}/moods`, { moodIds });
+  return data;
+};
+
+export const addBeatFeatures = async (beatId, featureIds) => {
+  const { data } = await axios.post(`${API_URL}/${beatId}/features`, { featureIds });
+  return data;
+};
+
+// Remove associations
+export const removeBeatKeyword = async (beatId, keywordId) => {
+  const { data } = await axios.delete(`${API_URL}/${beatId}/keywords/${keywordId}`);
+  return data;
+};
+
+export const removeBeatGenre = async (beatId, genreId) => {
+  const { data } = await axios.delete(`${API_URL}/${beatId}/genres/${genreId}`);
+  return data;
+};
+
+export const removeBeatMood = async (beatId, moodId) => {
+  const { data } = await axios.delete(`${API_URL}/${beatId}/moods/${moodId}`);
+  return data;
+};
+
+export const removeBeatFeature = async (beatId, featureId) => {
+  const { data } = await axios.delete(`${API_URL}/${beatId}/features/${featureId}`);
+  return data;
 };
