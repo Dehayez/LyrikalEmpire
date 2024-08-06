982b3ab86a56c0fa0e470de54afddbdbdc64a7a6
Dehayez <yentel.dh@hotmail.be>
2024-08-06 22:31:20 +0200

    cleaner code

--- client/src/index.js
+++ client/src/index.js
@@ -8,10 +8,7 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { PlaylistProvider } from './contexts/PlaylistContext'; 
-import { BeatProvider } from './contexts/BeatContext';
-import { DataProvider } from './contexts/DataContext';
-import { HeaderWidthProvider } from './contexts/HeaderWidthContext';
+import { PlaylistProvider, BeatProvider, DataProvider, HeaderWidthProvider } from './contexts'; 
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
