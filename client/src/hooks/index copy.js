2ce8c9d9a2ef9cf845f5bbe2e8c289dcb69a71ec
Dehayez <yentel.dh@hotmail.be>
2024-08-15 16:39:30 +0200

    update README.md

--- client/src/hooks/index copy.js
+++
@@ -1,7 +0,0 @@
-export { useHandleBeatClick } from './useHandleBeatClick';
-export { useBeatActions } from './useBeatActions';
-export { handlePlay, handleNext, handlePrev } from './audioControls';
-export { useResizableColumns } from './useResizableColumns';
-export { useBpmHandlers } from './useBpmHandlers';
-export { useSelectableList } from './useSelectableList';
-export { useSelectableListWithUpdate } from './useSelectableListWithUpdate';
