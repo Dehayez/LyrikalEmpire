59fd48c1d23f15751f2f668c2bd32e0bde88678b
Dehayez <yentel.dh@hotmail.be>
2024-08-20 03:46:58 +0200

    History/Queue: ContextMenu position upgrade

--- client/src/components/Queue/Queue.js
+++ client/src/components/Queue/Queue.js
@@ -40,19 +40,14 @@ const Queue = ({ queue, setQueue, currentBeat, onBeatClick, isShuffleEnabled, cu
     e.stopPropagation();
     const button = e.currentTarget;
   
-    const historyListElement = document.querySelector('.queue');
+    const buttonRect = button.getBoundingClientRect();
+    const contextMenuWidth = 200;
+    const adjustedX = buttonRect.right - contextMenuWidth;
+    const adjustedY = buttonRect.top + buttonRect.height;
   
-    if (historyListElement) {
-      const { left, top } = historyListElement.getBoundingClientRect();
-  
-      const buttonRect = button.getBoundingClientRect();
-      const adjustedX = buttonRect.left - left + buttonRect.width - 170;
-      const adjustedY = buttonRect.top - top + 100; 
-  
-      setActiveContextMenu(`${beat.id}-${index}`);
-      setContextMenuX(adjustedX);
-      setContextMenuY(adjustedY);
-    }
+    setActiveContextMenu(`${beat.id}-${index}`);
+    setContextMenuX(adjustedX);
+    setContextMenuY(adjustedY);
   };
 
   const handleBeatClick = (beat) => {
