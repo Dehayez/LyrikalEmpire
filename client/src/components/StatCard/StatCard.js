b343ee8245c3e2095638f0630c181cc854178730
Dehayez <yentel.dh@hotmail.be>
2024-08-21 06:41:33 +0200

    Dashboard: styling cards

--- client/src/components/StatCard/StatCard.js
+++ client/src/components/StatCard/StatCard.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './StatCard.scss';
 
-const StatCard = ({ title, value }) => {
+const StatCard = ({ title, value, size }) => {
   return (
-    <div className="stat-card">
+    <div className={`stat-card stat-card--${size}`}>
       <h2>{title}</h2>
       <p>{value}</p>
     </div>
@@ -14,6 +14,11 @@ const StatCard = ({ title, value }) => {
 StatCard.propTypes = {
   title: PropTypes.string.isRequired,
   value: PropTypes.number.isRequired,
+  size: PropTypes.oneOf(['large', 'small']),
+};
+
+StatCard.defaultProps = {
+  size: 'small',
 };
 
 export default StatCard;
