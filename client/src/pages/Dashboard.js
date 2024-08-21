9942a922e932cd3b5e9177cedac2f833b350d3e0
Dehayez <yentel.dh@hotmail.be>
2024-08-21 06:41:33 +0200

    Dashboard: styling cards

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -12,12 +12,12 @@ const Dashboard = () => {
     <div className="dashboard">
       <h1>Dashboard</h1>
       <div className="stats">
-        <StatCard title="Total Beats" value={allBeats.length} />
-        <StatCard title="Total Playlists" value={playlists.length} />
-        <StatCard title="Total Genres" value={genres.length} />
-        <StatCard title="Total Moods" value={moods.length} />
-        <StatCard title="Total Keywords" value={keywords.length} />
-        <StatCard title="Total Features" value={features.length} />
+        <StatCard size="large" title="Total Beats" value={allBeats.length} />
+        <StatCard size="large" title="Total Playlists" value={playlists.length} />
+        <StatCard size="small" title="Total Genres" value={genres.length} />
+        <StatCard size="small" title="Total Moods" value={moods.length} />
+        <StatCard size="small" title="Total Keywords" value={keywords.length} />
+        <StatCard size="small" title="Total Features" value={features.length} />
       </div>
     </div>
   );
