54a978cba12719a2bd1fe39c2343721a0dde3608
Dehayez <yentel.dh@hotmail.be>
2024-08-21 05:55:31 +0200

    Dashboard: new page

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Here you can see all the stats of the application.</p>
+      {/* Add your stats and other components here */}
+    </div>
+  );
+};
+
+export default Dashboard;
