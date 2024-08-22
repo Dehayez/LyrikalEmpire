5f3dc0010aeae25a7a2fa38a9f2465fef5fa120b
Dehayez <yentel.dh@hotmail.be>
2024-08-22 12:45:06 +0200

    Dashboard: charts in page / StatChart: filterOption

--- client/src/pages/FeaturesPage.js
+++ client/src/pages/FeaturesPage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useData } from '../contexts';
+import { StatCard, StatChart } from '../components';
+
 import '../globals/pageStyles.scss';
 
 const FeaturesPage = () => {
   const { features } = useData();
 
   return (
-    <div>
-      <h1 className="page-title">All Features</h1>
+    <div className="page">
+      <h1 className="page-title">Features</h1>
+      <StatCard size="full-width">
+        <StatChart filterOption="features" />
+      </StatCard>
       <ul className="page-list">
         {features.map((feature) => (
           <li key={feature.id} className="page-list-item">{feature.name}</li>
--- client/src/pages/GenresPage.js
+++ client/src/pages/GenresPage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useData } from '../contexts';
+import { StatCard, StatChart } from '../components';
+
 import '../globals/pageStyles.scss';
 
 const GenresPage = () => {
   const { genres } = useData();
 
   return (
-    <div>
-      <h1 className="page-title">All Genres</h1>
+    <div className="page">
+      <h1 className="page-title">Genres</h1>
+      <StatCard size="full-width">
+        <StatChart filterOption="genres" />
+      </StatCard>
       <ul className="page-list">
         {genres.map((genre) => (
           <li key={genre.id} className="page-list-item">{genre.name}</li>
