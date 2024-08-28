cfeba99efc90d73c8414299bec0ab5bf4d60357a
Dehayez <yentel.dh@hotmail.be>
2024-08-28 11:31:59 +0200

    Server: routes using dbHelper

--- server/routes/moods.js
+++ server/routes/moods.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db');
+const { handleQuery } = require('../helpers/dbHelpers');
 
-// Get Moods
 router.get('/', (req, res) => {
-  db.query('SELECT * FROM moods', (err, results) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'An error occurred while fetching moods' });
-    } else {
-      res.json(results);
-    }
-  });
+  handleQuery('SELECT * FROM moods', [], res, null, true);
 });
 
-// Add Mood
 router.post('/', (req, res) => {
   const { name } = req.body;
 
@@ -22,41 +13,18 @@ router.post('/', (req, res) => {
     return res.status(400).json({ error: 'Name is required' });
   }
 
-  db.query('INSERT INTO moods (name) VALUES (?)', [name], (err, results) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'An error occurred while adding the mood' });
-    } else {
-      res.status(201).json({ message: 'Mood added successfully', moodId: results.insertId });
-    }
-  });
+  handleQuery('INSERT INTO moods (name) VALUES (?)', [name], res, 'Mood added successfully');
 });
 
-// Update Mood
 router.put('/:id', (req, res) => {
   const { name } = req.body;
   const { id } = req.params;
-  db.query('UPDATE moods SET name = ? WHERE id = ?', [name, id], (err, results) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'An error occurred while updating the mood' });
-    } else {
-      res.json({ message: 'Mood updated successfully' });
-    }
-  });
+  handleQuery('UPDATE moods SET name = ? WHERE id = ?', [name, id], res, 'Mood updated successfully');
 });
 
-// Delete Mood
-router.delete('/api/moods/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  db.query('DELETE FROM moods WHERE id = ?', [id], (err, results) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'An error occurred while deleting the mood' });
-    } else {
-      res.json({ message: 'Mood deleted successfully' });
-    }
-  });
+  handleQuery('DELETE FROM moods WHERE id = ?', [id], res, 'Mood deleted successfully');
 });
 
 module.exports = router;
