80555b9af8664afc75cff53e3a9df7a25d175335
Dehayez <yentel.dh@hotmail.be>
2024-08-28 13:21:08 +0200

    Server: handleTransaction dbHelper

--- server/controllers/playlistController.js
+++ server/controllers/playlistController.js
@@ -45,23 +45,11 @@ const updatePlaylist = (req, res) => {
 
 const deletePlaylist = (req, res) => {
   const { id } = req.params;
-
-  dbHelpers.handleQuery(
-    'DELETE FROM playlists_beats WHERE playlist_id = ?',
-    [id],
-    res,
-    'All beats removed from playlist successfully'
-  ).then(() => {
-    dbHelpers.handleQuery(
-      'DELETE FROM playlists WHERE id = ?',
-      [id],
-      res,
-      'Playlist deleted successfully'
-    );
-  }).catch(err => {
-    console.error(err);
-    res.status(500).json({ error: 'An error occurred while deleting the playlist' });
-  });
+  const queries = [
+    { query: 'DELETE FROM playlists_beats WHERE playlist_id = ?', params: [id] },
+    { query: 'DELETE FROM playlists WHERE id = ?', params: [id] }
+  ];
+  dbHelpers.handleTransaction(queries, res, 'Playlist and all associated beats deleted successfully');
 };
 
 const addBeatToPlaylist = (req, res) => {
@@ -84,98 +72,16 @@ const removeBeatFromPlaylist = (req, res) => {
   );
 };
 
-const getBeatsInPlaylist = (req, res) => {
-  const { playlist_id } = req.params;
-  dbHelpers.handleQuery(
-    'SELECT b.*, pb.beat_order FROM beats b INNER JOIN playlists_beats pb ON b.id = pb.beat_id WHERE pb.playlist_id = ? ORDER BY pb.beat_order',
-    [playlist_id],
-    res,
-    null,
-    true
-  );
-};
-
-const addBeatsToPlaylist = (req, res) => {
-  const { playlist_id } = req.params;
-  const { beatIds } = req.body;
-
-  const insertValues = beatIds.map(beatId => [playlist_id, beatId]);
-
-  db.beginTransaction(err => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'An error occurred starting the transaction' });
-    }
-
-    db.query('UPDATE playlists_beats SET beat_order = beat_order + 1 WHERE playlist_id = ?', [playlist_id], (err) => {
-      if (err) {
-        return db.rollback(() => {
-          res.status(500).json({ error: 'An error occurred while updating the beat order' });
-        });
-      }
-
-      const checkAndInsertPromises = insertValues.map(([playlistId, beatId]) =>
-        new Promise((resolve, reject) => {
-          db.query('SELECT 1 FROM playlists_beats WHERE playlist_id = ? AND beat_id = ?', [playlistId, beatId], (err, results) => {
-            if (err) {
-              return reject(err);
-            }
-            if (results.length === 0) {
-              db.query('INSERT INTO playlists_beats (playlist_id, beat_id, beat_order) VALUES (?, ?, 0)', [playlistId, beatId], (err) => {
-                if (err) {
-                  return reject(err);
-                }
-                resolve();
-              });
-            } else {
-              resolve();
-            }
-          });
-        })
-      );
-
-      Promise.all(checkAndInsertPromises)
-        .then(() => {
-          db.commit(err => {
-            if (err) {
-              console.error(err);
-              db.rollback(() => {
-                res.status(500).json({ error: 'An error occurred while committing the transaction' });
-              });
-            } else {
-              res.status(201).json({ message: 'Beats added to playlist successfully, duplicates avoided' });
-            }
-          });
-        })
-        .catch(err => {
-          console.error(err);
-          db.rollback(() => {
-            res.status(500).json({ error: 'An error occurred while adding the beats to the playlist' });
-          });
-        });
-    });
-  });
-};
-
 const updateBeatOrderInPlaylist = (req, res) => {
   const { playlist_id } = req.params;
   const { beatOrders } = req.body;
 
-  const queries = beatOrders.map(({ id, order }) => {
-    return dbHelpers.handleQuery(
-      'UPDATE playlists_beats SET beat_order = ? WHERE playlist_id = ? AND beat_id = ?',
-      [order, playlist_id, id],
-      res,
-      null
-    );
-  });
+  const queries = beatOrders.map(({ id, order }) => ({
+    query: 'UPDATE playlists_beats SET beat_order = ? WHERE playlist_id = ? AND beat_id = ?',
+    params: [order, playlist_id, id]
+  }));
 
-  Promise.all(queries)
-    .then(() => res.json({ message: 'Beat order updated successfully' }))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'An error occurred while updating the beat order' });
-    });
+  dbHelpers.handleTransaction(queries, res, 'Beat order updated successfully');
 };
 
 const removeAllBeatsFromPlaylist = (req, res) => {
@@ -188,6 +94,29 @@ const removeAllBeatsFromPlaylist = (req, res) => {
   );
 };
 
+const getBeatsInPlaylist = (req, res) => {
+  const { playlist_id } = req.params;
+  dbHelpers.handleQuery(
+    'SELECT * FROM beats WHERE id IN (SELECT beat_id FROM playlists_beats WHERE playlist_id = ?)',
+    [playlist_id],
+    res,
+    'Beats in playlist fetched successfully',
+    true
+  );
+};
+
+const addBeatsToPlaylist = (req, res) => {
+  const { playlist_id } = req.params;
+  const { beatIds } = req.body;
+
+  const queries = beatIds.map(beat_id => ({
+    query: 'INSERT INTO playlists_beats (playlist_id, beat_id) VALUES (?, ?)',
+    params: [playlist_id, beat_id]
+  }));
+
+  dbHelpers.handleTransaction(queries, res, 'Beats added to playlist successfully');
+};
+
 module.exports = {
   createPlaylist,
   getPlaylists,
@@ -196,8 +125,8 @@ module.exports = {
   deletePlaylist,
   addBeatToPlaylist,
   removeBeatFromPlaylist,
-  getBeatsInPlaylist,
-  addBeatsToPlaylist,
   updateBeatOrderInPlaylist,
-  removeAllBeatsFromPlaylist
+  removeAllBeatsFromPlaylist,
+  getBeatsInPlaylist,
+  addBeatsToPlaylist
 };
--- server/helpers/dbHelpers.js
+++ server/helpers/dbHelpers.js
@@ -2,7 +2,7 @@ const db = require('../config/db');
 
 const handleQuery = async (query, params, res, successMessage, returnResultsOnly = false) => {
   try {
-    const [results] = await db.promise().query(query, params);
+    const [results] = await db.query(query, params);
     if (returnResultsOnly) {
       return res.json(results);
     }
@@ -13,4 +13,22 @@ const handleQuery = async (query, params, res, successMessage, returnResultsOnly
   }
 };
 
-module.exports = { handleQuery };
+const handleTransaction = async (queries, res, successMessage) => {
+  const connection = await db.getConnection();
+  try {
+    await connection.beginTransaction();
+    for (const { query, params } of queries) {
+      await connection.query(query, params);
+    }
+    await connection.commit();
+    res.json({ message: successMessage });
+  } catch (err) {
+    await connection.rollback();
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while processing the transaction' });
+  } finally {
+    connection.release();
+  }
+};
+
+module.exports = { handleQuery, handleTransaction };
