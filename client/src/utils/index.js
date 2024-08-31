693d5b698ec22730fc6a2e4030b4897a3f32bcf4
Dehayez <yentel.dh@hotmail.be>
2024-09-01 01:52:37 +0200

    formDuration util

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -3,4 +3,5 @@ export { apiConfig };
 export { isMobileOrTablet } from './isMobileOrTablet';
 export { eventBus } from './EventBus';
 export { sortBeats } from './sortBeats';
+export { formatDuration } from './formatDuration';
 export * from './stateUtils';
