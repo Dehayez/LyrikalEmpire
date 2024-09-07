14b2d0180a1b16f5179a7ca6e8c5c62a15f3ee58
Dehayez <yentel.dh@hotmail.be>
2024-09-07 19:02:26 +0200

    Functionality: replace audiofile

--- server/routes/beatRoute.js
+++ server/routes/beatRoute.js
@@ -8,7 +8,7 @@ router.post('/', upload.single('audio'), beatController.createBeat);
 router.get('/:id', beatController.getBeatById);
 router.put('/:id', beatController.updateBeat);
 router.delete('/:id', beatController.deleteBeat);
-router.put('/:id', upload.single('audio'), beatController.replaceAudio);
+router.put('/:id/audio', upload.single('audio'), beatController.replaceAudio);
 router.post('/:beat_id/:association_type', beatController.addAssociation);
 router.delete('/:beat_id/:association_type/:association_id', beatController.removeAssociation);
 router.get('/:beat_id/:association_type', beatController.getAssociations);
