096862d261347976a5f54b9b95dc6e192d3b11b1
Dehayez <yentel.dh@hotmail.be>
2024-10-16 22:16:40 +0200

    Html Classname Enter fix

--- client/src/components/Buttons/Button.js
+++ client/src/components/Buttons/Button.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Button.scss';
 
 const Button = ({ className, onClick, children, text, type = 'button', variant, size }) => {
-  const buttonClass = `
-    button 
-    ${className} 
-    ${variant === 'primary' ? 'button--primary' : ''} 
-    ${variant === 'transparent' ? 'button--transparent' : ''}
-    ${variant === 'warning' ? 'button--warning' : ''}
-    ${size ? `button--${size}` : ''} 
-  `;
+  const buttonClass = [
+    'button',
+    className,
+    variant === 'primary' && 'button--primary',
+    variant === 'transparent' && 'button--transparent',
+    variant === 'warning' && 'button--warning',
+    size && `button--${size}`
+  ].filter(Boolean).join(' ');
 
   return (
     <button className={buttonClass.trim()} onClick={onClick} type={type}>
