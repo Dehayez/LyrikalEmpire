c90933b325dc69406ed14bf480c1391f67f3353a
Dehayez <yentel.dh@hotmail.be>
2024-10-27 10:39:52 +0100

    tesing fails fixed

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,5 +1,15 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { TextEncoder, TextDecoder } from 'util';
+
+configure({ adapter: new Adapter() });
+
+// Polyfill TextEncoder and TextDecoder
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+// Create a div element with id 'root' and append it to the document body
+const root = document.createElement('div');
+root.setAttribute('id', 'root');
+document.body.appendChild(root);
