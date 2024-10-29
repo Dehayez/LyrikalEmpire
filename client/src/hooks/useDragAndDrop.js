a62427507d07e965c6fa873eacd90aa90023eebc
Dehayez <yentel.dh@hotmail.be>
2024-10-29 20:25:44 +0100

    useDragAndDrop: react import

--- client/src/hooks/useDragAndDrop.js
+++ client/src/hooks/useDragAndDrop.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { IoCloseSharp, IoCheckmarkSharp } from "react-icons/io5";
