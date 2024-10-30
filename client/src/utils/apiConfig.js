de38ed0203e011f9b30996333cddbaaed42826ce
Dehayez <yentel.dh@hotmail.be>
2024-10-30 16:41:33 +0100

    app/beatlist padding fix

--- client/src/utils/apiConfig.js
+++ client/src/utils/apiConfig.js
@@ -1,2 +1,3 @@
-const API_BASE_URL = 'https://6e8d-2a02-1812-c0c-e400-f1b5-4b8-d7f2-42a4.ngrok-free.app';
+//const API_BASE_URL = 'http://localhost:4000';
+const API_BASE_URL = 'https://5629-2a02-1812-c0c-e400-ed-9356-385b-1541.ngrok-free.app';
 export default API_BASE_URL;
