c27aa237801f50045d90aa1b16b2e73c8d75775d
Dehayez <yentel.dh@hotmail.be>
2024-11-11 12:26:28 +0100

    lyricsService: DRY update

--- client/src/services/lyricsService.js
+++ client/src/services/lyricsService.js
@@ -1,34 +1,17 @@
-import axios from 'axios';
 import API_BASE_URL from '../utils/apiConfig';
+import { apiRequest } from '../utils/apiUtils';
 
 const API_URL = `${API_BASE_URL}/api/lyrics`;
 
 export const getLyricsById = async (id) => {
-  try {
-    const response = await axios.get(`${API_URL}/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error(`Failed to fetch lyric with ID ${id}:`, error);
-    throw error;
-  }
+  return await apiRequest('get', `/${id}`, API_URL);
 };
 
 export const updateLyricsById = async (id, lyrics) => {
-  try {
-    const response = await axios.put(`${API_URL}/${id}`, { lyrics });
-    return response.data;
-  } catch (error) {
-    console.error(`Failed to update lyric with ID ${id}:`, error);
-    throw error;
-  }
+  return await apiRequest('put', `/${id}`, API_URL, { lyrics });
 };
 
 export const createLyrics = async (lyrics) => {
-    try {
-      const response = await axios.post(`${API_URL}`, { lyrics });
-      return response.data.results.insertId;
-    } catch (error) {
-      console.error('Failed to create lyrics:', error);
-      throw error;
-    }
-  };
+  const response = await apiRequest('post', '', API_URL, { lyrics });
+  return response.results.insertId;
+};
