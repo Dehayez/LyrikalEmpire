2ba4490e2d5fcf7313ef6d456ee691c966a664a0
Dehayez <yentel.dh@hotmail.be>
2024-12-18 16:45:48 +0100

    Token refresh fix

--- server/controllers/tokenController.js
+++ server/controllers/tokenController.js
@@ -29,8 +29,6 @@ const refreshToken = async (req, res) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
-    const accessToken = generateAccessToken({ id: decoded.id, email: decoded.email });
-
     const now = Math.floor(Date.now() / 1000);
     const exp = decoded.exp;
     const oneDayInSeconds = 24 * 60 * 60;
@@ -43,6 +41,8 @@ const refreshToken = async (req, res) => {
       storedRefreshToken = newRefreshToken;
       console.log(`[INFO] Refresh Token refreshed: ${newRefreshToken}`);
     }
+
+    const accessToken = generateAccessToken({ id: decoded.id, email: decoded.email });
 
     if (res) {
       res.json({ accessToken, refreshToken: newRefreshToken });
@@ -58,13 +58,6 @@ const refreshToken = async (req, res) => {
     }
   }
 };
-
-// Simulate storing the refresh token after user login
-storedRefreshToken = generateRefreshToken({ id: 1, email: 'test@example.com' });
-console.log(`[INFO] Initial Stored Refresh Token: ${storedRefreshToken}`);
-
-// Refresh access token every 14 minutes (for testing purposes)
-setInterval(() => refreshToken(null, null), 14 * 60 * 1000);
 
 module.exports = {
   generateAccessToken,
