ce88f90db623c7de7ad772ccfc225fc3f50d9b75
Dehayez <yentel.dh@hotmail.be>
2025-01-02 17:30:52 +0100

    Updated api base url

--- client/src/utils/apiConfig.js
+++ client/src/utils/apiConfig.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = 'http://localhost:4000';
+const API_BASE_URL = 'http://174.138.4.195';
+//const API_BASE_URL = 'http://localhost:4000';
 //const API_BASE_URL = 'https://5629-2a02-1812-c0c-e400-ed-9356-385b-1541.ngrok-free.app';
 export default API_BASE_URL;
