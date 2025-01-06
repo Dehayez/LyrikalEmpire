530a6ee09372fdcc34b03330504037057887c14c
Dehayez <yentel.dh@hotmail.be>
2025-01-06 22:46:30 +0100

    userController: login logs

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -148,6 +148,12 @@ const login = async (req, res) => {
     return res.status(400).json({ error: 'Email/Username and password are required' });
   }
 
+  console.log('DB_HOST:', process.env.DB_HOST);
+  console.log('DB_USER:', process.env.DB_USER);
+  console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+  console.log('DB_NAME:', process.env.DB_NAME);
+  console.log('DB_PORT:', process.env.DB_PORT);
+
 try {
     const [user] = await db.query(
       'SELECT * FROM users WHERE email = ? OR username = ?',
