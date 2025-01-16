4db578abf519dcc9393e98914c44108fa2085132
Dehayez <yentel.dh@hotmail.be>
2025-01-16 23:07:00 +0100

    Cleanup DB Fixes

--- server/config/testDbConnection.js
+++ server/config/testDbConnection.js
@@ -1,12 +1,6 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_PORT:', process.env.DB_PORT);
-
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -14,8 +8,5 @@ const db = mysql.createPool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });
-
-console.log('DB:', db);
-console.log('DB:', db());
 
 module.exports = db;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-require('dotenv').config({ path: '../.env' });
 
 const express = require('express');
 const session = require('express-session');
