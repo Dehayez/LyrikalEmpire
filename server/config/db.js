ac6f0e3eeadc2e9a3997e00edb27b20e43353d0e
Dehayez <yentel.dh@hotmail.be>
2025-01-16 22:16:38 +0100

    Database User Connection Test

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
+const user = process.env.DB_USER;
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  user: user,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 const express = require('express');
 const session = require('express-session');
