286d744f60d59508519b684b36f733a6dbc801c9
Dehayez <yentel.dh@hotmail.be>
2025-01-22 22:20:14 +0100

    Icons/favicon Build configuration

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -45,6 +46,18 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: path.resolve(__dirname, '../public'), 
+          to: path.resolve(__dirname, '../build'), 
+          globOptions: { 
+            ignore: ['**/index.html', '**/uploads/**'] 
+          } 
+        },
+      ],
     }),
   ],
   devtool: 'source-map',
