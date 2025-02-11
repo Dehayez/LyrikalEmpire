a55337f59bea5e44a84fe5f890b6454e9713bcf6
Dehayez <yentel.dh@hotmail.be>
2025-02-11 20:42:09 +0100

    Development mode

--- client/src/utils/apiConfig.js
+++ client/src/utils/apiConfig.js
@@ -1,7 +1,6 @@
 // Local Server
-const API_BASE_URL = 'http://localhost:4000/api';
+//const API_BASE_URL = 'http://localhost:4000/api';
 
 // Third Server
-//const API_BASE_URL = 'http://174.138.4.195:4000';
-//const API_BASE_URL = 'https://www.lyrikalempire.com/api';
+const API_BASE_URL = 'https://www.lyrikalempire.com/api';
 export default API_BASE_URL;
