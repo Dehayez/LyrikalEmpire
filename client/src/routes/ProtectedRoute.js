fe1b9cf214ad343da4f9ab09ad9054d51779fceb
Dehayez <yentel.dh@hotmail.be>
2025-03-21 21:52:03 +0100

    Loading spinner

--- client/src/routes/ProtectedRoute.js
+++ client/src/routes/ProtectedRoute.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useUser } from '../contexts/UserContext';
+import { ClipLoader } from 'react-spinners';
 
 const ProtectedRoute = ({ element }) => {
   const { isAuthenticated, isLoading } = useUser();
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className="spinner-container"><ClipLoader size={50} color={"#FFCC44"} loading={isLoading} /></div>;
   }
 
   return isAuthenticated ? element : <Navigate to="/login" />;
