dd9b97e89f882730f2fca623fb4e3f80672ed213
Dehayez <yentel.dh@hotmail.be>
2025-04-02 16:48:28 +0200

    SelectableInput UI

--- client/src/components/Inputs/SelectableInput.js
+++ client/src/components/Inputs/SelectableInput.js
@@ -183,6 +183,10 @@ export const SelectableInput = ({ beatId, associationType, headerIndex, label, p
     }
   }, [newBeatId, form, associationType, pendingAssociations]);
 
+  useEffect(() => {
+    console.log(isFocused);
+  }, [isFocused]);
+
   return (
     <div className='selectable-input-container'>
       <div className={`selectable-input ${label ? 'selectable-input--label' : ''}`} ref={containerRef}>
