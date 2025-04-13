1dfee8a81aae23e08d91df8b0c3132a7b70a8769
Dehayez <yentel.dh@hotmail.be>
2025-04-13 16:31:29 +0200

    Accessibility update

--- client/src/components/Inputs/SelectableInput.js
+++ client/src/components/Inputs/SelectableInput.js
@@ -208,30 +208,33 @@ export const SelectableInput = ({
             {label}
           </label>
           <input
-            ref={inputRef}
-            id={`selectable-input-${associationType}-${beatId}-${headerIndex}`}
-            className={classNames('form-group__input', 'selectable-input__input', {
-              'selectable-input__input--hidden': !isFocused,
-              'selectable-input__input--disabled': disableFocus,
-            })}
-            placeholder={selectedItems.length === 0 ? placeholder : ''}
-            type="text"
-            value={inputValue}
-            onFocus={(e) => {
-              handleFocus();
-              e.target.previousSibling.classList.add('form-group__label--active');
-            }}
-            onBlur={(e) => {
-              handleBlur(e);
-              if (!inputValue && selectedItems.length === 0) {
-                e.target.previousSibling.classList.remove('form-group__label--active');
-              }
-            }}
-            onChange={handleInputChange}
-            onKeyDown={handleKeyDown}
-            onClick={(e) => e.stopPropagation()}
-            disabled={disableFocus}
-            autoComplete="off"
+              ref={inputRef}
+              id={`selectable-input-${associationType}-${beatId}-${headerIndex}`}
+              className={classNames('form-group__input', 'selectable-input__input', {
+                  'selectable-input__input--hidden': !isFocused,
+                  'selectable-input__input--disabled': disableFocus,
+              })}
+              placeholder={selectedItems.length === 0 ? placeholder : ''}
+              type="text"
+              value={inputValue}
+              onFocus={(e) => {
+                  handleFocus();
+                  e.target.previousSibling.classList.add('form-group__label--active');
+              }}
+              onBlur={(e) => {
+                  handleBlur(e);
+                  if (!inputValue && selectedItems.length === 0) {
+                      e.target.previousSibling.classList.remove('form-group__label--active');
+                  }
+              }}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+              onClick={(e) => e.stopPropagation()}
+              disabled={disableFocus}
+              autoComplete="off"
+              tabIndex={!isFocused ? -1 : 0}
+              aria-hidden={!isFocused}
+              aria-label={label || placeholder}
           />
         </div>
         {isFocused && (
