00ceb643c634657abf05c4357319acab63ab2826
Dehayez <yentel.dh@hotmail.be>
2025-04-22 14:45:53 +0200

    Build

--- client/src/components/BeatList/TableHeader.js
+++ client/src/components/BeatList/TableHeader.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { isMobileOrTablet } from '../../utils';
 import { useResizableColumns } from '../../hooks';
