e59574b95ae9e8a6253ff5220cab621bceee0cf0
Dehayez <yentel.dh@hotmail.be>
2025-05-28 12:35:21 +0200

    Removed unused imports

--- client/src/contexts/BeatContext.js
+++ client/src/contexts/BeatContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import { getBeats } from '../services';
 import { useUser } from '../contexts';
 
